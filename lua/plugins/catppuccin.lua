9ebb2590a79364bd3df5e49222ebad151f61189c
mohammed-shakir <mohammedshakir0105282gmail.com>
2025-08-20 16:14:33 +0200

    bug

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,12 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    init = function()
+      local ok, m = pcall(require, "catppuccin.groups.integrations.bufferline")
+      if ok and m and m.get_theme and m.get == nil then
+        m.get = m.get_theme
+      end
+    end,
     opts = {
       custom_highlights = function(colors)
         return {
